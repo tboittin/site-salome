ac1f5991f8a141ff6b14a53075180b7e4f1a30a6
Thomas Boittin <tboittin@gmail.com>
2023-04-16 17:32:51 +0200

    navbar scroll detection, responsive font size and fized position

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
 const hamburger = document.getElementById('hamburger');
 const navbar = document.getElementById('navbar');
 const navbarItems = document.querySelectorAll('.navbar-item');
+const navbarLinks = navbar.querySelectorAll('a');
+
+// Fonction qui change la classe active de la navbar en fonction de la section visible
+function setActiveNav() {
+  // Récupération de la section visible en fonction de la position du scroll
+  const sections = document.querySelectorAll('.section');
+  let visibleSection;
+  sections.forEach((section) => {
+    if (section.getBoundingClientRect().top <= 0) {
+      visibleSection = section.getAttribute('id');
+    }
+  });
+  // Parcours des liens de la navbar pour ajouter ou supprimer la classe active
+  navbarLinks.forEach((link) => {
+    if (link.getAttribute('href').substring(1) === visibleSection) {
+      link.classList.add('active');
+    } else {
+      link.classList.remove('active');
+    }
+  });
+}
+
+// Ajout d'un écouteur d'événement pour mettre à jour la navbar lors d'un scroll
+window.addEventListener('scroll', setActiveNav);
 
 hamburger.addEventListener('click', function() {
   this.classList.toggle('active');
